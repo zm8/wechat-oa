0a08f2a91a583492804549d778ce11c5ffce8ddc
zhengming <zhengming@zhengmingdeMacBook-Pro.local>
2025-01-09 17:22:25 +0800

    feat: 虚拟滚动的代码例子提交

---
+++ examples/infinite-list/simple/src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { faker } from "@faker-js/faker";
+import { ref } from "vue";
+import InfiniteListAdvance from "./components/InfiniteList/index.vue";
+
+const data = ref<{ value: string; uid: number }[]>([]);
+
+for (let i = 100; i < 1000; i++) {
+  data.value.push({
+    uid: i,
+    value: faker.lorem.sentences(),
+  });
+}
+</script>
+
+<template>
+  <InfiniteListAdvance :listData="data" :itemSize="100"></InfiniteListAdvance>
+</template>
