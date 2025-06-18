2adf66384e778d70af63ffd90464b8c08afa6217
David.Zheng <280541464@qq.com>
2025-06-18 17:04:36 +0800

    refactor: vxe-table-test 测试组件里面, VxeTable 引用的组件修改。

--- examples/vxe-table-build/vxe-table-test/src/App.vue
+++ examples/vxe-table-build/vxe-table-test/src/App.vue
@@ -1,11 +1,11 @@
 <script setup>
-import VxeTable_3_15_34 from "./components/VxeTable_3_15_34.vue";
+import VxeTable_3_latest from "./components/VxeTable_3_latest.vue";
 import VxeTable_3_5_9 from "./components/VxeTable_3_5_9.vue";
 </script>
 
 <template>
   <div id="app">
-    <VxeTable_3_15_34 />
+    <VxeTable_3_latest />
     <div style="height: 40px"></div>
     <VxeTable_3_5_9 />
   </div>
