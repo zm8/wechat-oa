a205d0fee4d6869976784692262c4c01d0398ec1
郑铭 <280541464@qq.com>
2026-09-04 09:55:36 +0800

    feat: vue3-dist 首次代码提交

---
+++ examples/vue3-pdfjs/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import PdfVirtual from './components/PdfVirtual.vue'
+import PdfOfficial from './components/PdfOfficial.vue'
+</script>
+
+<template>
+  <div>
+    <PdfVirtual />
+    <PdfOfficial />
+  </div>
+</template>
---
+++ examples/vue3-pdfjs/src/components/PdfVirtual.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    PdfVirtual
+  </div>
+</template>
